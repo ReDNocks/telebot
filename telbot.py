78dd2ab5db46721ca354d5f27a8d115ca7ac8d1c
ReDWaR <goldbes23@gmail.com>
2023-08-23 01:07:15 +0300

    f

--- telbot.py
+++ telbot.py
@@ -5,13 +5,13 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from telebot.types import InputMediaPhoto
 import sqlite3 as sl
 from functools import partial
+from io import BytesIO
 
-bot = telebot.TeleBot('6673879527:AAGKIM0bC1Aqqk2uhKkx5w71Yupa2WBYYhg');
+
+
+bot = telebot.TeleBot('6673879527:AAGKIM0bC1Aqqk2uhKkx5w71Yupa2WBYYhg')
 global user
 user = []
-
-#-1001980704979
-
 
 def create_keyboard_1():
     markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
@@ -54,7 +54,6 @@ def create_keyb_4(boon,dish):
     keyb_2.add(five_btn)
     return keyb_2
 
-
 def create_keyb_5():
     keyb_5 = InlineKeyboardMarkup()
     one_btn = types.InlineKeyboardButton(text="Изменить кол-во", callback_data='8')
@@ -78,6 +77,23 @@ def adminusers(a):
     keyb_users_rem.add(one_btn, two_btn, three_btn,four_btn)
     return keyb_users_rem
 
+def admindish(a):
+    keyb_dish = InlineKeyboardMarkup()
+    one_btn = types.InlineKeyboardButton(text="Изменить имя", callback_data='s1' + str(a))
+    two_btn = types.InlineKeyboardButton(text="Изменить вес", callback_data='s2' + str(a))
+    three_btn = types.InlineKeyboardButton(text="Изменить стоимость", callback_data='s3' + str(a))
+    four_btn = types.InlineKeyboardButton(text="Изменить описание", callback_data='s4' + str(a))
+    five_btn = types.InlineKeyboardButton(text="Изменить фото", callback_data='s5' + str(a))
+    six_btn = types.InlineKeyboardButton(text="Изменить категорию", callback_data='s6' + str(a))
+    seven_btn = types.InlineKeyboardButton(text="Изменить время приготовления", callback_data='s7' + str(a))
+    eight_btn = types.InlineKeyboardButton(text="Изменить рейтинг", callback_data='s8' + str(a))
+    nine_btn = types.InlineKeyboardButton(text="Поставить или снять 'STOP'", callback_data='s9' + str(a))
+    keyb_dish.add(one_btn, two_btn, three_btn, four_btn,five_btn)
+    keyb_dish.add(six_btn, seven_btn, eight_btn,nine_btn)
+    return keyb_dish
+
+
+
 
 # Запуск бота и проверка на регистрацию
 @bot.message_handler(commands=['start'])
@@ -99,12 +115,11 @@ def start(message):
                          text="Добро пожаловать в бот ресторан нажми кнопку регистрации чтобы продолжить.", reply_markup=create_keyboard_1())
             bot.register_next_step_handler(regstr,reg)
 
-
 bbb = {}
 user_bbb = {}
 
 # БЛОК АДМИН ПАНЕЛИ
-@bot.message_handler(commands=['admin','adminusers','admincateg'])
+@bot.message_handler(commands=['admin','adminusers','admincateg','adminorders','admindish'])
 
 def admin(message):
     post_levl_1 =['Доступные комманды суперадмина','Редактирование пользователей /adminusers',
@@ -114,8 +129,7 @@ def admin(message):
                   'Редактирорвание категорий блюд /admincateg','Редактирование блюд /admindish',
                   'Редактирование заказов /adminorders']
     con = sl.connect('tgbase.db')
-    post = con.execute(f"SELECT post FROM USERS").fetchall()
-
+    post = con.execute(f"SELECT post FROM USERS WHERE id_telegram = {message.from_user.id}").fetchall()
     if message.text =="/admin":
         if post[0][0] == 1:
             v = '\n'.join(post_levl_1)
@@ -143,11 +157,22 @@ def admin(message):
             keyb_categ.add(one_btn, two_btn)
             bot.send_message(message.chat.id, text=f'Выберите действие', reply_markup=keyb_categ)
 
+    elif message.text =="/adminorders":
+        if post[0][0] == 1 or post[0][0] == 2:
+            orders = con.execute(f"SELECT id, user FROM ORDERS ").fetchall()
+            keyb_orders = InlineKeyboardMarkup()
+            for i in range(len(orders)):
+                keyb_orders.add(
+                    InlineKeyboardButton("№ заказа " + str(orders[i][0]) + " id_user" + str(orders[i][1]), callback_data="e" + str(orders[i][0])))
+            bot.send_message(message.chat.id, text=f'Выберите заказ', reply_markup=keyb_orders)
 
-
-
-
-
+    elif message.text =="/admindish":
+        if post[0][0] == 1 or post[0][0] == 2:
+            keyb_dish = InlineKeyboardMarkup()
+            one_btn = types.InlineKeyboardButton(text="Добавить новое блюдо", callback_data='hv1')
+            two_btn = types.InlineKeyboardButton(text="Изменить существующее блюдо", callback_data='hv2')
+            keyb_dish.add(one_btn, two_btn)
+            bot.send_message(message.chat.id, text=f'Выберите действие', reply_markup=keyb_dish)
 
 @bot.message_handler(content_types=['text'])
 # Регистрация
@@ -196,9 +221,8 @@ def reg(message):
         bot.register_next_step_handler(fol, reg)
         return
 
-
+# Меню
 def menu(message):
-    # Меню
     answer = bot.send_message(message.chat.id, text="Выберисте категорию",reply_markup=menu_gen())
 
 # Корзина
@@ -272,7 +296,7 @@ def support(message):
     create_keyboard_2()
 
 
-#Редактирование Имени(панель админа)
+#Редактирование Пользователей (панель админа)
 def users_change(message):
     con = sl.connect('tgbase.db')
     if user_bbb['c'][0] == "1":
@@ -293,7 +317,7 @@ def users_change(message):
             con.execute(f"UPDATE USERS SET tel = '{message.text}' WHERE id_telegram = {int(user_bbb['c'][1:])}")
         bot.send_message(message.chat.id, text=f" Телефон изменён на {message.text} Для возврата в меню админа /admin")
 
-#Редактирование и создание категорий блюд
+#Редактирование и создание категорий блюд (панель администратора)
 def categ_change(message):
     con = sl.connect('tgbase.db')
     with con:
@@ -306,12 +330,68 @@ def categ_add(message):
         con.execute(f"INSERT OR IGNORE INTO CATEGORY (name) values('{message.text}')")
     bot.send_message(message.chat.id, text=f"Категория успешно создана с именем {message.text} Для возврата в меню админа /admin")
 
+# Редактирование заказов(панель админа)
+def orders_change(message):
+    con = sl.connect('tgbase.db')
+    with con:
+        con.execute(f"UPDATE ORDERS SET payment = {int(message.text)} WHERE id = {int(user_bbb['e'])}")
+    bot.send_message(message.chat.id,text=f"Оплата для заказа № {user_bbb['e']} изменена. Для возврата в меню админа /admin")
 
 
+# Редактирование блюд
+def dish_change(message):
+    con = sl.connect('tgbase.db')
+    if user_bbb['s'][0] == "1":
+        with con:
+            con.execute(f"UPDATE DISHES SET name = '{message.text}' WHERE id = {int(user_bbb['s'][1])}")
+        bot.send_message(message.chat.id,text=f"Имя блюда изменено на {message.text}. Для возврата в меню админа /admin")
 
+    elif user_bbb['s'][0] == "2":
+        with con:
+            con.execute(f"UPDATE DISHES SET weight = '{message.text}' WHERE id = {int(user_bbb['s'][1])}")
+        bot.send_message(message.chat.id,text=f"Вес блюда изменен на {message.text}. Для возврата в меню админа /admin")
 
+    elif user_bbb['s'][0] == "3":
+        with con:
+            con.execute(f"UPDATE DISHES SET price = '{message.text}' WHERE id = {int(user_bbb['s'][1])}")
+        bot.send_message(message.chat.id,text=f"Стоимость блюда изменена на {message.text}. Для возврата в меню админа /admin")
 
+    elif user_bbb['s'][0] == "4":
+        with con:
+            con.execute(f"UPDATE DISHES SET description = '{message.text}' WHERE id = {int(user_bbb['s'][1])}")
+        bot.send_message(message.chat.id,text=f"Описание блюда изменено. Для возврата в меню админа /admin")
 
+    elif user_bbb['s'][0] == "5":
+        print("В разраб")
+        photo_id = message.photo[-1].file_id
+        photo_file = bot.get_file(photo_id)
+        photo_bytes = bot.download_file(photo_file.file_path)
+        ste = BytesIO(photo_bytes)
+        # image = Image.open(stream).convert("RGBA")
+        # ste.close()
+        with con:
+            con.execute(f"UPDATE DISHES SET photo = {ste} WHERE id = {int(user_bbb['s'][1])}")
+        bot.send_message(message.chat.id,text=f"Фото блюда изменено. Для возврата в меню админа /admin")
+
+    elif user_bbb['s'][0] == "6":
+        with con:
+            con.execute(f"UPDATE DISHES SET category = {int(message.text)} WHERE id = {int(user_bbb['s'][1])}")
+        bot.send_message(message.chat.id,text=f"Категория блюда изменена. Для возврата в меню админа /admin")
+
+    elif user_bbb['s'][0] == "7":
+        with con:
+            con.execute(f"UPDATE DISHES SET cooking_time = {message.text} WHERE id = {int(user_bbb['s'][1])}")
+        bot.send_message(message.chat.id,text=f"Время приготовления блюда изменено. Для возврата в меню админа /admin")
+
+    elif user_bbb['s'][0] == "8":
+        with con:
+            con.execute(f"UPDATE DISHES SET rating = {int(message.text)} WHERE id = {int(user_bbb['s'][1])}")
+        bot.send_message(message.chat.id,text=f"Рейтинг блюда изменен. Для возврата в меню админа /admin")
+
+    elif user_bbb['s'][0] == "9":
+        with con:
+            con.execute(f"UPDATE DISHES SET stoped = {int(message.text)} WHERE id = {int(user_bbb['s'][1])}")
+        bot.send_message(message.chat.id,text=f"'Stop' блюда изменен. Для возврата в меню админа /admin")
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -444,18 +524,37 @@ def query_handler(call):
             v = bot.send_message(call.message.chat.id, text=f'Введите новое имя для категории')
             bot.register_next_step_handler(v, categ_change)
 
+    if flag == "e":
+        user_bbb['e'] = data
+        x = bot.send_message(call.message.chat.id, text=f'Введите статус оплаты где 0-Не оплачен, 1-Оплачен')
+        bot.register_next_step_handler(x, orders_change)
 
 
+    if flag == "h":
+        if call.data[1:] == "v1":
+            f = bot.send_message(call.message.chat.id, text=f'даров')
+            # bot.register_next_step_handler(f, categ_add)
 
+        elif call.data[1:] =="v2":
+            dish = con.execute(f"SELECT id, name FROM DISHES ").fetchall()
+            keyb_dish = InlineKeyboardMarkup()
+            for i in range(len(dish)):
+                keyb_dish.add(
+                    InlineKeyboardButton("Блюдо № " + str(dish[i][0]) + " " + str(dish[i][1]), callback_data="k" + str(dish[i][0])))
+            bot.send_message(call.message.chat.id, text=f'Выберите блюдо', reply_markup=keyb_dish)
 
+    if flag == "k":
+        bot.send_message(call.message.chat.id, text=f'Выберите действие', reply_markup=admindish(data))
 
-
-
-
-
-
-
-
+    if flag == "s":
+        user_bbb['s'] = data
+        if data[0] == "9":
+            user_bbb['s'] = data
+            y = bot.send_message(call.message.chat.id, text=f'Введите значение для "STOP" где 0-Блюдо в меню 1-Блюдо на стопе')
+            bot.register_next_step_handler(y, dish_change)
+        else:
+            y = bot.send_message(call.message.chat.id, text=f'Введите изменения(или загрузите фото)')
+            bot.register_next_step_handler(y, dish_change)
 
 
 
